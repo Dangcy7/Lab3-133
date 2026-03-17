a7cae7c27cba1f0732603c5b9d89adec850858ec
Dangcy7 <danyyaniquez7@gmail.com>
2026-03-16 20:24:22 -0400

    Archivos iniciales lista primo presupuesto

---
+++ Primo.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+
+public class Primo{
+    public static void main(String[]args){
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Introduzca un numero para verificar si es primo: ");
+
+        int primo=sc.nextInt();
+
+        boolean esPrimo=true;
+
+        if (primo==1 ){
+            esPrimo=false;
+        }else{
+            for(int i=2; i <= primo / 2;i++){
+
+                if (primo % i == 0){
+
+                    esPrimo=false;
+
+                    break;
+                }
+
+            }
+        }
+
+        if (esPrimo ){
+            System.out.println("El numero es primo");
+        }else{
+            System.out.println("El numero no es primo");
+        }
+        
+    }
+    
+}
+
